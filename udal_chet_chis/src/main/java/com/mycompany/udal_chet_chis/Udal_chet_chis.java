0655e4f44dd4a3a05463a27a8970cfe1da1685b4
Ju3lia <Ju3lia@users.noreply.github.com>
2022-12-05 18:26:08 +0700

    Th

--- udal_chet_chis/src/main/java/com/mycompany/udal_chet_chis/Udal_chet_chis.java
+++ udal_chet_chis/src/main/java/com/mycompany/udal_chet_chis/Udal_chet_chis.java
@@ -14,9 +14,22 @@ import java.util.Scanner;
 public class Udal_chet_chis {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner (System.in);
-        System.out.println("Введите число: ");
-        int x= in.nextInt();
-        
+        int num = 0; 
+        int rez = 0; 
+        int pow10 = 1; 
+            Scanner myScnr = new Scanner(System.in);
+            System.out.println("Введите начальное число:");
+            num = myScnr.nextInt();
+                while (num!=0)
+                    {if (num % 2 != 0)
+                    {rez = rez + num % 10 * pow10;
+                     pow10 = pow10*10;   
+                    }
+                     num = num/10;
+                    }
+               
+       System.out.println(rez);}
     }
-}
+        
+
+
